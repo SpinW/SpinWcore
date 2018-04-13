e5e11f0ce586b4f9aab772e432a73ed7d5a02741
Simon Ward <substance33@gmail.com>
2018-04-13 16:26:36 +0200

    Cosmetic changes

--- tests/swsym/swsymTest.cpp
+++ tests/swsym/swsymTest.cpp
@@ -8,21 +8,24 @@
 using namespace std;
 const char actualDir[] = "/MATLAB/mtools/SpinW_Dev/spinw/dat_files/";
 
-class symResults{
+class symResults {
 public:
     arma::cube Expected;
     int Index;
-    arma::cube Result(){
+
+    arma::cube Result() {
         return thisSym.getSym(Index);
     }
-    explicit symResults(int ind){
-        switch (ind){
-            case 0:{
-                arma::cube temp(3,4,1,arma::fill::zeros);
+
+    explicit symResults(int ind) {
+        switch (ind) {
+            case 0: {
+                arma::cube temp(3, 4, 1, arma::fill::zeros);
                 temp.slice(0) = {{1, 0, 0, 0},
-                                     {0, 1, 0, 0},
-                                     {0, 0, 1, 0}};
-                Expected = temp;}
+                                 {0, 1, 0, 0},
+                                 {0, 0, 1, 0}};
+                Expected = temp;
+            }
                 break;
             case 9: {
                 arma::cube temp(3, 4, 2, arma::fill::zeros);
@@ -31,8 +34,8 @@ public:
                                  {0,  0, -1, 0}};
 
                 temp.slice(1) = {{-1, 0,  0,  0},
-                                     {0,  -1, 0,  0},
-                                     {0,  0,  -1, 0}};
+                                 {0,  -1, 0,  0},
+                                 {0,  0,  -1, 0}};
                 Expected = temp;
             }
                 break;
@@ -40,16 +43,16 @@ public:
                 arma::cube temp(3, 4, 3, arma::fill::zeros);
 
                 temp.slice(0) = {{1, 0, 0, 0.5},
-                                     {0, 1, 0, 0.5},
-                                     {0, 0, 1, 0.0}};
+                                 {0, 1, 0, 0.5},
+                                 {0, 0, 1, 0.0}};
 
                 temp.slice(1) = {{-1, 0,  0, 0.0},
-                                     {0,  -1, 0, 0.0},
-                                     {0,  0,  1, 0.5}};
+                                 {0,  -1, 0, 0.0},
+                                 {0,  0,  1, 0.5}};
 
                 temp.slice(2) = {{-1, 0, 0,  0.0},
-                                     {0,  1, 0,  0.0},
-                                     {0,  0, -1, 0.5}};
+                                 {0,  1, 0,  0.0},
+                                 {0,  0, -1, 0.5}};
                 Expected = temp;
             }
                 break;
@@ -57,16 +60,16 @@ public:
                 arma::cube temp(3, 4, 3, arma::fill::zeros);
 
                 temp.slice(0) = {{1, 0, 0, 0.0},
-                                     {0, 1, 0, 0.5},
-                                     {0, 0, 1, 0.5}};
+                                 {0, 1, 0, 0.5},
+                                 {0, 0, 1, 0.5}};
 
                 temp.slice(1) = {{-1, 0,  0, 0.0},
-                                     {0,  -1, 0, 0.0},
-                                     {0,  0,  1, 0.0}};
+                                 {0,  -1, 0, 0.0},
+                                 {0,  0,  1, 0.0}};
 
-                temp.slice(2) = {{    1, 0,  0,  0.5},
-                                     {0, -1, 0,  0.0},
-                                     {0, 0,  1, 0.0}};
+                temp.slice(2) = {{1, 0,  0, 0.5},
+                                 {0, -1, 0, 0.0},
+                                 {0, 0,  1, 0.0}};
                 Expected = temp;
             }
                 break;
@@ -74,16 +77,16 @@ public:
                 arma::cube temp(3, 4, 3, arma::fill::zeros);
 
                 temp.slice(0) = {{1, 0, 0, 0.5},
-                                     {0, 1, 0, 0.5},
-                                     {0, 0, 1, 0.5}};
+                                 {0, 1, 0, 0.5},
+                                 {0, 0, 1, 0.5}};
 
                 temp.slice(1) = {{-1, 0,  0, 0.5},
-                                     {0,  -1, 0, 0.5},
-                                     {0,  0,  1, 0.5}};
+                                 {0,  -1, 0, 0.5},
+                                 {0,  0,  1, 0.5}};
 
                 temp.slice(2) = {{0, -1, 0, 0.0},
-                                     {1, 0,  0, 0.5},
-                                     {0, 0,  1, 0.25}};
+                                 {1, 0,  0, 0.5},
+                                 {0, 0,  1, 0.25}};
                 Expected = temp;
             }
                 break;
@@ -91,40 +94,41 @@ public:
                 arma::cube temp(3, 4, 3, arma::fill::zeros);
 
                 temp.slice(0) = {{1, 0, 0, 1.0 / 3},
-                                     {0, 1, 0, 2.0 / 3},
-                                     {0, 0, 1, 2.0 / 3}};
+                                 {0, 1, 0, 2.0 / 3},
+                                 {0, 0, 1, 2.0 / 3}};
 
                 temp.slice(1) = {{0, -1, 0, 0.0},
-                                     {1, -1, 0, 0.0},
-                                     {0, 0,  1, 0.0}};
+                                 {1, -1, 0, 0.0},
+                                 {0, 0,  1, 0.0}};
 
                 temp.slice(2) = {{0,  -1, 0, 0.0},
-                                     {-1, 0,  0, 0.0},
-                                     {0,  0,  1, 0.0}};
+                                 {-1, 0,  0, 0.0},
+                                 {0,  0,  1, 0.0}};
                 Expected = temp;
             }
                 break;
         }
         Index = ind;
     }
+
 protected:
     char tempAdr[256];
-    swsym thisSym = swsym(strcpy(tempAdr,string(actualDir).c_str()));
+    swsym thisSym = swsym(strcpy(tempAdr, string(actualDir).c_str()));
 };
 
 // A new one of these is create for each test
-class SwSymTestClassLoc : public testing::TestWithParam<const char*>
-{
+class SwSymTestClassLoc : public testing::TestWithParam<const char *> {
 public:
-    virtual void SetUp(){}
-    virtual void TearDown(){}
+    virtual void SetUp() {}
+
+    virtual void TearDown() {}
 };
 
-class SwSymTestClassSym : public testing::TestWithParam<symResults>
-{
+class SwSymTestClassSym : public testing::TestWithParam<symResults> {
 public:
-    virtual void SetUp(){}
-    virtual void TearDown(){}
+    virtual void SetUp() {}
+
+    virtual void TearDown() {}
 };
 
 INSTANTIATE_TEST_CASE_P(DatDirs,
@@ -144,19 +148,18 @@ INSTANTIATE_TEST_CASE_P(SymPos,
                                           symResults(159)
                         ));
 
-TEST_P(SwSymTestClassLoc, acceptsDir)
-{
+TEST_P(SwSymTestClassLoc, acceptsDir) {
     const char *dir = GetParam();
     string temp = string(dir);
     char blah[256];
     try {
-        swsym* thisSym = new swsym(strcpy(blah,temp.c_str()));
-    } catch (const std::exception &e){
+        swsym *thisSym = new swsym(strcpy(blah, temp.c_str()));
+    } catch (const std::exception &e) {
         cout << temp << endl;
         EXPECT_THROW({
-            EXPECT_STREQ("Unable to open dat file",e.what());
-            throw;
-        },std::exception);
+                         EXPECT_STREQ("Unable to open dat file", e.what());
+                         throw;
+                     }, std::exception);
     }
 }
 
@@ -164,7 +167,7 @@ TEST_P(SwSymTestClassSym, testSymResult) {
 
     symResults thisSymResults = GetParam();
 
-    arma::cube Result_C   = thisSymResults.Result();
+    arma::cube Result_C = thisSymResults.Result();
     arma::cube Expected_C = thisSymResults.Expected;
 
     std::vector<double> Result(Result_C.memptr(),
@@ -175,5 +178,5 @@ TEST_P(SwSymTestClassSym, testSymResult) {
 //    for (size_t ind = 0; ind < Result.size(); ind++){
 //        EXPECT_DOUBLE_EQ(Result[ind],Expected[ind]);
 //    }
-    EXPECT_EQ(Result,Expected);
+    EXPECT_EQ(Result, Expected);
 }
